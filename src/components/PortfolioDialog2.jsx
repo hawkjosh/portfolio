83df9a411d5218cab270e6e535a42cbd0c42900c
Josh Hawk <hawkjosh@icloud.com>
2022-12-17 13:46:10 -0500

    completed refactoring update

--- src/components/PortfolioDialog2.jsx
+++ src/components/PortfolioDialog2.jsx
@@ -15,108 +15,153 @@ const theme = createTheme({
   breakpoints: {
     values: {
       // Breakpoints below use vertical layout
-      xxs: 0,     // Change to 'zero'
-      xs: 375,     // DELETE THIS ONE
-      sm: 428,     // Change to 'mobile'
-      md: 768,     // Change to 'tabletSmall'
-      tabletLarge: 820,     // ADD THIS ONE
+      zero: 0,
+      mobile: 428,
+      tabletSmall: 768,
+      tabletLarge: 820,
       // Breakpoints below change to horizontal layout
-      lg: 1024,     // Change to 'laptopSmall: 1263'
-      xl: 1440,     // Change to 'laptopLarge: 1519'
-      xxl: 1620     // DELETE THIS ONE
+      laptopSmall: 1263,
+      laptopLarge: 1519
     }
   }
 })
 
-// const Laptop = ({ children }) => {
-//   const isLaptop = useMediaQuery({ minWidth: 1000 })
-//   return isLaptop ? children : null
-// }
+const Laptop = ({ children }) => {
+  const isLaptop = useMediaQuery({ minWidth: 1000 })
+  return isLaptop ? children : null
+}
 
-// const Tablet = ({ children }) => {
-//   const isTablet = useMediaQuery({ minWidth: 551, maxWidth: 999 })
-//   return isTablet ? children : null
-// }
+const Tablet = ({ children }) => {
+  const isTablet = useMediaQuery({ minWidth: 551, maxWidth: 999 })
+  return isTablet ? children : null
+}
 
-// const Mobile = ({ children }) => {
-//   const isMobile = useMediaQuery({ maxWidth: 550 })
-//   return isMobile ? children : null
-// }
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 550 })
+  return isMobile ? children : null
+}
 
-export default function App() {
+export default function PortfolioDialog2() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}
-      >
+
+      {/* for laptop/desktop displays (horizontal layout) */}
+
+      <Laptop>
         <Box
-          component='img'
-          src={messageForum}
-          alt="Message Forum App"
           sx={{
-            border: '4px solid white',
-            borderRadius: '10px',
-            marginBottom: {
-              md: '0.75rem'
-            },
-            width: {
-              xxs: '90%',
-              md: '75%',
-              lg: '65%',
-              xl: '60%'
-            }
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center'
           }}
-        />
-        <Typography
+          >
+          <Box
+            component='img'
+            src={messageForum}
+            alt="Message Forum App"
+            sx={{
+              border: '4px solid white',
+              borderRadius: '10px',
+              marginBottom: '0.75rem',
+              width: { laptopSmall: '65%', laptopLarge: '60%' }
+            }}
+          />
+          <Typography
+            sx={{
+              color: 'white',
+              width: { laptopSmall: '65%', laptopLarge: '60%' },
+              marginTop: '1.25rem',
+              textAlign: 'justify',
+              fontWeight: '300',
+              fontSize: { laptopSmall: '1.35rem', laptopLarge: '1.45rem' },
+              lineHeight: { laptopSmall: '1.65rem', laptopLarge: '1.75rem' },
+              letterSpacing: { laptopSmall: '0.3rem', laptopLarge: '0.4rem' }
+            }}
+            >
+            A unique and fun message forum app that follows the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the Express-Session npm package for authentication.
+          </Typography>
+        </Box>
+      </Laptop>
+
+      {/* for tablet displays (vertical layout) */}
+
+      <Tablet>
+        <Box
           sx={{
-            color: 'white',
-            width: {
-              xxs: '90%',
-              md: '75%',
-              lg: '65%',
-              xl: '60%'
-            },
-            marginTop: '1.25rem',
-            textAlign: 'justify',
-            fontWeight: '300',
-            fontSize: {
-              xxs: '0.9rem',
-              xs: '1rem',
-              sm: '1.1rem',
-              md: '1.25rem',
-              lg: '1.35rem',
-              xl: '1.45rem',
-              xxl: '1.5rem'
-            },
-            lineHeight: {
-              xxs: '1.25rem',
-              xs: '1.35rem',
-              sm: '1.45rem',
-              md: '1.5rem',
-              lg: '1.65rem',
-              xl: '1.75rem',
-              xxl: '1.85rem'
-            },
-            letterSpacing: {
-              xxs: '0.07rem',
-              xs: '0.15rem',
-              sm: '0.22rem',
-              md: '0.25rem',
-              lg: '0.3rem',
-              xl: '0.4rem',
-              xxl: '0.45rem'
-            }
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center'
           }}
-        >
-          A unique and fun message forum app that follows the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the Express-Session npm package for authentication.
-        </Typography>
-      </Box>
+          >
+          <Box
+            component='img'
+            src={messageForum}
+            alt="Message Forum App"
+            sx={{
+              border: '4px solid white',
+              borderRadius: '10px',
+              marginBottom: '0.75rem',
+              width: '75%'
+            }}
+          />
+          <Typography
+            sx={{
+              color: 'white',
+              width: '75%',
+              marginTop: '1.25rem',
+              textAlign: 'justify',
+              fontWeight: '300',
+              fontSize: '1.25rem',
+              lineHeight: '1.5rem',
+              letterSpacing: '0.25rem'
+            }}
+            >
+            A unique and fun message forum app that follows the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the Express-Session npm package for authentication.
+          </Typography>
+        </Box>
+      </Tablet>
+
+      {/* for mobile displays (vertical layout) */}
+
+      <Mobile>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+          >
+          <Box
+            component='img'
+            src={messageForum}
+            alt="Message Forum App"
+            sx={{
+              border: '4px solid white',
+              borderRadius: '10px',
+              width: '90%'
+            }}
+          />
+          <Typography
+            sx={{
+              color: 'white',
+              width: '90%',
+              marginTop: '1.25rem',
+              textAlign: 'justify',
+              fontWeight: '300',
+              fontSize: { zero: '0.9rem', mobile: '1.1rem' },
+              lineHeight: { zero: '1.25rem', mobile: '1.45rem' },
+              letterSpacing: { zero: '0.07rem', mobile: '0.22rem' }
+            }}
+            >
+            A unique and fun message forum app that follows the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the Express-Session npm package for authentication.
+          </Typography>
+        </Box>
+      </Mobile>
+
     </ThemeProvider>
   )
 }
